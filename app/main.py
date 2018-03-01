6160f0263e581819ca96198d7e41c7a259b9f82d
Thaer Khawaja <thaer.khawaja@gmail.com>
2018-02-28 19:35:20 -0800

    Simplify the logic down a bit.

--- app/main.py
+++ app/main.py
@@ -67,13 +67,8 @@ def map_move(snake, point):
 
 # trim_tip necessary due to data anomaly we're getting from the server
 # unable to fully remove because many nodes are in same position, can collide if too close (hence > 1 condition)
-def find_path(board, start, end, trim_tip=False):
-    if trim_tip and board.pt_distance(start, end) > 1:
-        board_c = deepcopy(board)
-        board_c[end.y][end.x] = None
-        grid = Grid(matrix=board_c)
-    else:
-        grid = Grid(matrix=board)
+def find_path(board, start, end):
+    grid = Grid(matrix=board)
     start_pt = grid.node(start.x, start.y)
     end_pt = grid.node(end.x, end.y)
 
@@ -96,12 +91,13 @@ def get_cut_path(board, start_pt, end_pt):
     return path_candidate, cut_len
 
 
-def find_disjoint_path(board, path_init, snake, food):
+def find_disjoint_path(board, snake, food):
+    path_init = find_path(board, snake.head, food)
+    logger.debug('Init path: {}'.format(path_init))
+
     path_return = find_path(board, food, snake.tip)
     logger.debug('Return path: {}'.format(path_return))
-    if path_return == []:
-        path_return, _ = get_cut_path(board, food, snake.tip)
-        logger.debug('Trying a cut return path: {}'.format(path_return))
+
     intersects = set(path_init).intersection(set(path_return))
     logger.debug('Removing intersections: {}'.format(intersects))
     if intersects:
@@ -116,31 +112,36 @@ def find_disjoint_path(board, path_init, snake, food):
         path = disjoint_path
     else:
         path = path_init
-    return path
+
+    return_exists = True if path_return else False
+    return path, return_exists
 
 
 def get_move(board):
     snake = board.agent_snake
     food = board.food[0]  # TODO-TK: needs "closest food" functionality
 
-    path_init = find_path(board, snake.head, food)
-    logger.debug('Init path: {}'.format(path_init))
+    # find path with exits in consideration
+    path_init, return_exists = find_disjoint_path(board, snake, food)
 
-    # check if we can make a path assuming future snake length
-    if path_init:
-        # find path with exits in consideration
-        path_final = find_disjoint_path(board, path_init, snake, food)
+    if path_init and return_exists:
+        path_final = path_init
     else:
-        tailchase_path = find_path(board, snake.head, snake.tip, trim_tip=True)
+        tailchase_path = find_path(board, snake.head, snake.tip)
         logger.debug('Tail chase path: {}'.format(tailchase_path))
-        if tailchase_path:
+        if len(tailchase_path) > 1:
             logger.info('Using tail chase path')
             path_final = tailchase_path
         else:
-            # TODO-TK: need to implement a reasonable longest path approximation
-            cut_food_path, cut_food_len = get_cut_path(board, snake.head, food)
-            cut_tip_path, cut_tip_len = get_cut_path(board, snake.head, snake.tip)
-            path_final = cut_food_path if cut_food_len <= cut_tip_len else cut_tip_path
+            if path_init:
+                logger.info('Reverting to init path.')
+                path_final = path_init
+            else:
+                # TODO-TK: need to implement a reasonable longest path approximation
+                logger.info('Using a cut path.')
+                cut_food_path, cut_food_len = get_cut_path(board, snake.head, food)
+                cut_tip_path, cut_tip_len = get_cut_path(board, snake.head, snake.tip)
+                path_final = cut_food_path if cut_food_len <= cut_tip_len and cut_food_path else cut_tip_path
 
 
     logger.info("Using path: {}".format(path_final))
